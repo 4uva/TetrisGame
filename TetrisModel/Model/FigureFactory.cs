42c05a9484804829bf5fc8cf6c74e7b9f940c509
4uva <41539104+4uva@users.noreply.github.com>
2019-05-20 01:35:31 +0300

    created a common class for building figures of a game

---
+++ TetrisModel/Model/FigureFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetrisGameModel
+{
+    class FigureFactory
+    {
+        Cell Empty { get; }
+        Cell Occupied { get; }
+        public int width { get; }
+        public int heigh { get; }
+    }
+}
