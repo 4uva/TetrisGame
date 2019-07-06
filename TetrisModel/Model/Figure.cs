12c1513c80ef07d5373edcc1e7a298350a65ccc0
4uva <41539104+4uva@users.noreply.github.com>
2019-07-06 23:55:36 +0300

    Implemented figure and its rotation

--- TetrisModel/Model/Figure.cs
+++ TetrisModel/Model/Figure.cs
@@ -3,11 +3,49 @@ namespace TetrisGameModel
 {
     public class Figure
     {
-        public int width { get; }
-        public int heigh { get; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+        public Cell[][] Figurearray { get; private set; }
+
+        public Figure(int width, int height, Cell[][] figurearray)
+        {
+            Width = width;
+            Height = height;
+            Figurearray = figurearray;
+        }
+
+        public void Rotate()// todo check cell  out or in playground
+        {
+            int initialWidth = Width;
+            Width = Height;
+            Height = initialWidth;
+
+
+            Cell[][] allColumns = new Cell[Width][];//объявили создание  массива колонок
+
+            for (int i = 0; i < allColumns.Length; i += 1)//обошли индексы  массива колонок
+            {
+
+                var column = new Cell[Height];//*создали* столбец нужной длины
+                for (int j = 0; j < column.Length; j += 1)// мы обходим индексы в столбце
+                {
+                    int i_Old = j; //установили  начальные значения индексов  в старом массиве старого массива (горизонталь)
+                    int j_Old = Height - i;//установили начальные значения индексов  в старом массиве старого массива (вертикаль)
+
+                    column[j] = Figurearray[i_Old][j_Old];//столбец-массив выглядит как `Figurearray[i_Old]`
+
+                    
+                }
+                allColumns[i] = column;
+            }
+            Figurearray = allColumns;
+            
+        }
+
     }
 }
-        
-        
+
+
+
 
 
