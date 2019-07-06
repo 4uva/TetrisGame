1f8007394e07e0c931c584b50cb2e27fe246c474
4uva <41539104+4uva@users.noreply.github.com>
2019-07-07 00:06:12 +0300

    implemented figure factory for different figure variations

--- TetrisModel/Model/FigureFactory.cs
+++ TetrisModel/Model/FigureFactory.cs
@@ -4,11 +4,81 @@ using System.Text;
 
 namespace TetrisGameModel
 {
-    class FigureFactory
+    // Дизайн:
+    // 1. Класс FigureFactory отвечает за заполнение клеток в
+    //    создаваемых фигурах
+    // 2. Класс FigureFactory не требует от вызывающего кода
+    //    знания клеток фигур, это знание - ответственность
+    //    класса FigureFactory
+    // 3. Клетки являются энумами и как таковые не требуют своего
+    //    создания: мы можеми просто пользоваться литералами энума
+    // 4. Клетки будущей фигуры в каком-то виде хранятся в поле
+    //    FigureFactory
+    public class FigureFactory
     {
-        Cell Empty { get; }
-        Cell Occupied { get; }
-        public int width { get; }
-        public int heigh { get; }
+        public Figure CreateIFigure()
+        {
+            Cell[][] array = new Cell[1][];
+            array[0] = new Cell[] { Cell.Occupied, Cell.Occupied, Cell.Occupied, Cell.Occupied };
+
+            Figure figure = new Figure(1, 4, array);
+            return figure;
+        }
+        public Figure CreateQFigure()
+        {
+            Cell[][] array = new Cell[2][];
+            array[0] = new Cell[] { Cell.Occupied, Cell.Occupied };       
+            array[1] = new Cell[] { Cell.Occupied, Cell.Occupied }; 
+
+            Figure figure = new Figure(2, 2, array);
+            return figure;
+        }
+        public Figure CreateZFigure()
+        {
+            Cell[][] array = new Cell[2][];
+            array[0] = new Cell[] { Cell.Occupied, Cell.Occupied, Cell.Empty };
+            array[1] = new Cell[] { Cell.Empty, Cell.Occupied, Cell.Occupied };
+
+            Figure figure = new Figure(3, 2, array);
+            return figure;
+        }
+        public Figure CreateSFigure()
+        {
+            Cell[][] array = new Cell[2][];
+            array[0] = new Cell[] { Cell.Empty, Cell.Occupied, Cell.Occupied };
+            array[1] = new Cell[] { Cell.Occupied, Cell.Occupied, Cell.Empty };
+
+            Figure figure = new Figure(3, 2, array);
+            return figure;
+        }
+
+        public Figure CreateTFigure()
+        {
+            Cell[][] array = new Cell[2][];
+            array[0] = new Cell[] { Cell.Empty, Cell.Occupied, Cell.Empty };
+            array[1] = new Cell[] { Cell.Occupied, Cell.Occupied, Cell.Occupied };
+
+            Figure figure = new Figure(3, 2, array);
+            return figure;
+        }
+        public Figure CreateJFigure()
+        {
+            Cell[][] array = new Cell[2][];
+            array[0] = new Cell[] { Cell.Empty, Cell.Empty, Cell.Occupied };
+            array[1] = new Cell[] { Cell.Occupied, Cell.Occupied, Cell.Occupied };
+
+            Figure figure = new Figure( 2, 3, array);
+            return figure;
+        }
+        public Figure CreateLFigure()
+        {
+            Cell[][] array = new Cell[2][];
+            array[0] = new Cell[] { Cell.Occupied, Cell.Occupied, Cell.Occupied };       
+            array[1] = new Cell[] {  Cell.Empty, Cell.Empty,Cell.Occupied, }; 
+
+            Figure figure = new Figure(2, 3, array);
+            return figure;
+        }
     }
 }
+
--- TetrisModel/Model/Playground.cs
+++ TetrisModel/Model/Playground.cs
@@ -6,14 +6,48 @@ namespace TetrisGameModel
     {
         public int numberOfRows { get; }
         public int numberOfColumns { get; }
-        public Cell[,] playgroundarray { get; }
+        public Cell[,] Playgroundarray { get; }
         //  TODO:create own value type cell
+
         public Playground(int numberOfRows, int numberOfColumns)
         {
             numberOfRows = 22;
             numberOfColumns = 10;
 
-            playgroundarray = CreateArrayofCells();
+            Cell[,] playgroundarray = CreateArrayofCells();
+            Playgroundarray = playgroundarray;
+        }
+        void PutFigure(Figure figure)
+        {
+
+
+            if (figure.Width <= numberOfRows && figure.Height <= numberOfColumns)
+            {
+                for (int i = 0; i < figure.Width; i++)
+                {
+                    for (int j = 0; j <= figure.Height; j++)
+                    {
+                        Cell cellEmpty = Playgroundarray[i, j];
+                        if (cellEmpty != Cell.Empty)
+                        {
+                            throw new ArgumentException();
+                        }
+                    }
+                }
+
+                for (int i = 0; i < figure.Width; i++)
+                {
+                    for (int j = 0; j <= figure.Height; j++)
+                    {
+                        Cell figureCell = figure.Figurearray[i][j];
+                        Playgroundarray[i, j] = figureCell;
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
 
         Cell[,] CreateArrayofCells()
@@ -30,5 +64,6 @@ namespace TetrisGameModel
 
     }
 }
+
 
 
