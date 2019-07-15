8778e97e59d428cd2ba662da9a9a9d9ac295fefa
4uva <41539104+4uva@users.noreply.github.com>
2019-07-16 01:39:05 +0300

    Switched to multilayer design

--- TetrisModel/Model/Playground.cs
+++ TetrisModel/Model/Playground.cs
@@ -8,7 +8,50 @@ namespace TetrisGameModel
         public int numberOfRows { get; }
         public int numberOfColumns { get; }
         public Cell[,] Playgroundarray { get; }
+        Figure figureOnPlayGround;
         //  TODO:create own value type cell
+
+        Cell this[int i, int j]
+        {
+            get
+            {
+                Figure figure = figureOnPlayGround;
+                Cell playgroundCell = Playgroundarray[i, j];
+                Cell figureCell = figure.Figurearray[i][j];
+                if (figureCell != Cell.Empty)
+                {
+                    return Cell.WithFlyingFigure;
+                }
+                else
+                {
+                    return playgroundCell;
+                }
+            }
+        }
+
+        void PutFigure(Figure figure)
+        {
+            //figure height & width < playground size
+            if (figure.Width > numberOfRows || figure.Height > numberOfColumns)
+                throw new ArgumentException();
+
+            //figure puts itself with its occupied or 
+            //flying cell on occupied cells of playground
+            for (int i = 0; i < figure.Width; i++)
+            {
+                for (int j = 0; j <= figure.Height; j++)
+                {
+                    Cell playgroundCell = Playgroundarray[i, j];
+                    Cell figureCell = figure.Figurearray[i][j];
+
+                    if (figureCell != Cell.Empty && playgroundCell != Cell.Empty)
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+            }
+            figureOnPlayGround = figure;
+        }
 
         public Playground(int numberOfRows, int numberOfColumns)
         {
@@ -18,40 +61,17 @@ namespace TetrisGameModel
             Cell[,] playgroundarray = CreateArrayofCells();
             Playgroundarray = playgroundarray;
         }
-        void PutFigure(Figure figure)
-        {
-            if (figure.Width <= numberOfRows && figure.Height <= numberOfColumns)
-            {
-                for (int i = 0; i < figure.Width; i++)
-                {
-                    for (int j = 0; j <= figure.Height; j++)
-                    {
-                        Cell playgroundCell = Playgroundarray[i, j];
-                        Cell figureCell = figure.Figurearray[i][j];
-                        if (figureCell != Cell.Empty && playgroundCell != Cell.Empty)
-                        {
-                            throw new ArgumentException();
-                        }
-                    }
-                }
-
-                for (int i = 0; i < figure.Width; i++)
-                {
-                    for (int j = 0; j <= figure.Height; j++)
-                    {
-                        Cell figureCell = figure.Figurearray[i][j];
-                        if (figureCell != Cell.Empty)
-                        {
-                            Playgroundarray[i, j] = Cell.WithFlyingFigure;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                throw new ArgumentException();
-            }
-        }
+        //public Color this[int i, int j]//вопрос только в слове this
+        //{//Console.Write(nr.GetColor(i, j) + " ");
+        //    get
+        //    {
+        //        Color originalColor = originalColors[i, j];//это индексация массива
+        //        if (originalColor == Color.Red)
+        //            return Color.Yellow;
+        //        else
+        //            return originalColor;
+        //    }
+        //}
 
         Cell[,] CreateArrayofCells()
         {
@@ -65,7 +85,7 @@ namespace TetrisGameModel
             return array;
         }
     }
-}
+
 
 
 
