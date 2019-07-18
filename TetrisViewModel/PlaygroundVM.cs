9ae4fa8cc7ac5a0ad0c558519c3349a36a19b08c
4uva <41539104+4uva@users.noreply.github.com>
2019-07-18 17:44:16 +0300

    Added VM as a separate project

---
+++ TetrisViewModel/PlaygroundVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using TetrisGameModel;
+
+namespace TetrisViewModel
+{
+    class PlayGroundVM : BaseViewModel
+    {
+        PlayGroundVM()
+
+
+        {
+            Playground playgroud = new Playground(22, 10);
+
+        }
+    }
+}
